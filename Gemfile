5f5f18dbacf677fdee0adba4106794c76bf143f9
Valerie Mettler <valeriemettler@gmail.com>
2014-08-17 08:48:41 -0500

    modified gemfile

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ group :doc do
   gem 'sdoc', require: false
 end
 
+
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- spec/features/description_missing_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-feature 'Project manager creates TODO' do
-  scenario 'Unsuccessfully' do
-    visit new_todo_path
-    click_button 'Save'
-    expect( page ).to have_content('Your new TODO was NOT saved.  Please add a Description.')
-  end
-end
