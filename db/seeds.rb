82b7d034842995499799b6c34cd1afb8fa461817
valeriemettler <valeriemettler@gmail.com>
2014-08-17 21:07:07 +0000

    Added Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+ # Create a member
+ member = User.new(
+   name:     'Member User',
+   email:    'valeriemettler@gmail.com',
+   password: 'helloworld',
+ )
+ member.skip_confirmation!
+ member.save
+
+puts "Seed finished"
