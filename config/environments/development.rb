44da477414b91fb356ee4700d3fcf7e5d25606f6
Valerie Mettler <valeriemettler@gmail.com>
2014-09-20 20:45:31 -0500

    updated user profile buttons

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,4 +30,6 @@ Blocitoff::Application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.mailer_sender = "valeriemettler@gmail.com"
+  config.raise_delivery_errors = true
 end
